fee6f1b7a2bbfac72127e9089b995965740282a3
liyingpeng <liyingpeng.jjroc@bytedance.com>
2021-04-02 10:08:20 +0800

    script fenlei

--- python/RepoModelReplace/main.py
+++ python/RepoModelReplace/main.py
@@ -20,28 +20,31 @@ key_words = "[self publishModel]"
 key_words_re = "\[self publishModel\]"
 
 key_words_array = [
-    {"[self publishModel]": "\[self publishModel\]"},
-    {"[self publishViewModel]": "\[self publishViewModel\]"},
+    # {"[self publishModel]": "\[self publishModel\]"},
+    # {"[self publishViewModel]": "\[self publishViewModel\]"},
 
-    {"originalPublishModel": "originalPublishModel"},
-    {"originPublishModel": "originPublishModel"},
+    # {"originalPublishModel": "originalPublishModel"},
+    # {"originPublishModel": "originPublishModel"},
     {"publishModel": "publishModel"},
-    {"originalModel": "originalModel"},
-    {"sourceModel": "sourceModel"},
+    # {"originalModel": "originalModel"},
+    # {"sourceModel": "sourceModel"},
     {"publishViewModel": "publishViewModel"},
-    {"originUploadPublishModel": "originUploadPublishModel"},
-    {"originalPublishViewModel": "originalPublishViewModel"},
-    {"imagePublishModel": "imagePublishModel"},
+    {"model": "model"},
+    {"origin": "origin"},
+
+    # {"originUploadPublishModel": "originUploadPublishModel"},
+    # {"originalPublishViewModel": "originalPublishViewModel"},
+    # {"imagePublishModel": "imagePublishModel"},
 
 
-    {"providedPublishModel": "providedPublishModel"},
-    {"currentPublishModel": "currentPublishModel"},
-    {"willTransforToPublishModel": "willTransforToPublishModel"},
-    {"mvPublishViewModel": "mvPublishViewModel"},
-    {"mvPublishModel": "mvPublishModel"},
-    {"_mvPublishModel": "_mvPublishModel"},
-    {"textPublishModel": "textPublishModel"},
-    {"_textPublishModel": "_textPublishModel"},
+    # {"providedPublishModel": "providedPublishModel"},
+    # {"currentPublishModel": "currentPublishModel"},
+    # {"willTransforToPublishModel": "willTransforToPublishModel"},
+    # {"mvPublishViewModel": "mvPublishViewModel"},
+    # {"mvPublishModel": "mvPublishModel"},
+    # {"_mvPublishModel": "_mvPublishModel"},
+    # {"textPublishModel": "textPublishModel"},
+    # {"_textPublishModel": "_textPublishModel"},
 
 ]
 
